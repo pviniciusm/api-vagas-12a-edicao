1c52e3205e4d1f673ac674ed508fd92477fb2a99
pviniciusm <pvinics@gmail.com>
2023-08-09 19:13:17 -0300

    Add redis config
    Co-authored-by: Eva Tavares <EvaTavares@users.noreply.github.com>
    Co-authored-by: David Chavier <David-Chavier@users.noreply.github.com>
    Co-authored-by: Gabriela Leichtweis <Gabileichtweis@users.noreply.github.com>
    Co-authored-by: Roberto Carvalho de Souza Veloso <Roberto1501@users.noreply.github.com>
    Co-authored-by: Igor Matos <Sarkan-DF@users.noreply.github.com>
    Co-authored-by: Jonas Silva <jonas-amilton@users.noreply.github.com>
    Co-authored-by: Assis Júnior <AssFerj@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import "reflect-metadata";
 import { Database } from "./main/database/database.connection";
 import { Server } from "./main/server/express.server";
+import { CacheDatabase } from "./main/database/redis.connection";
 
-Database.connect().then(() => {
-    console.log("Database is connected");
+Promise.all([Database.connect(), CacheDatabase.connect()]).then(() => {
     Server.listen();
 });
+
+// Database.connect().then(() => {
+//     console.log("Database is connected");
+//     Server.listen();
+// });
