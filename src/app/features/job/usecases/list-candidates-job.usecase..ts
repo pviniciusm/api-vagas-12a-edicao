0aec41647cbc88760c59d500ef85221319edd12b
Igor Matos <sarkan.igor@gmail.com>
2023-08-09 22:27:26 -0300

    implementação cache list-candidates-job

--- src/app/features/job/usecases/list-candidates-job.usecase..ts
+++ src/app/features/job/usecases/list-candidates-job.usecase..ts
@@ -1,3 +1,4 @@
+import { CacheRepository } from "../../../shared/database/repositories/cache.repository";
 import { Result, Usecase, UsecaseResponse } from "../../../shared/util";
 import { JobRepository } from "../repositories/job.repository";
 
@@ -8,8 +9,21 @@ interface listCandidatesJob {
 
 export class ListCandidatesJob implements Usecase {
   public async execute(params: listCandidatesJob): Promise<Result> {
-    // 1 - buscar a vaga pelo id => repository
+    // busca lista de vaga pela cache.
+    const cacheRepository = new CacheRepository();
+    const cacheResult = await cacheRepository.get(`candidate-${params.idJob}`);
 
+    if (cacheResult) {
+      return {
+        ok: true,
+        code: 200,
+        message:
+          "Candidates from the Job application successfully listed (cache!)",
+        data: cacheResult,
+      };
+    }
+
+    // 1 - buscar a vaga pelo id => repository
     const repository = new JobRepository();
     const result = await repository.getById(params.idJob);
 
@@ -23,6 +37,8 @@ export class ListCandidatesJob implements Usecase {
     if (result.job.idRecruiter !== params.idRecruiter) {
       return UsecaseResponse.unauthorized();
     }
+
+    await cacheRepository.set(`candidate-${params.idJob}`, result);
 
     // 4 - retornar vaga com os candidatos
     return {
